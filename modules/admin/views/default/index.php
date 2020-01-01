f5633bdf76c506b4808581213381208077918164
cosmastar112 <cosmastar112@gmail.com>
2020-01-01 20:29:24 +0400

    Сгенерирован CRUD модели авторов
    На главную страницу админки добавлена ссылка на раздел.

--- modules/admin/views/default/index.php
+++ modules/admin/views/default/index.php
@@ -1,12 +1,10 @@
 <div class="admin-default-index">
-    <h1><?= $this->context->action->uniqueId ?></h1>
-    <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
-    </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
+<?php 
+    use yii\helpers\Html;
+    use yii\helpers\Url;
+
+    echo Html::tag('a', 'Авторы', [ 
+        'href' => Url::toRoute('author/index')
+    ]);
+?>
 </div>
