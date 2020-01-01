c5b7da478506dc686baf6bf04ceb9e1514dfd8de
cosmastar112 <cosmastar112@gmail.com>
2020-01-01 21:10:16 +0400

    Сгенерирован CRUD модели книг
    На главную страницу админки добавлена ссылка на раздел.

---
+++ modules/admin/models/Book.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace app\modules\admin\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "book".
+ *
+ * @property int $id
+ * @property string $title
+ * @property int $author_id
+ *
+ * @property Author $author
+ * @property Register[] $registers
+ */
+class Book extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'book';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'author_id'], 'required'],
+            [['author_id'], 'integer'],
+            [['title'], 'string', 'max' => 120],
+            [['author_id'], 'exist', 'skipOnError' => true, 'targetClass' => Author::className(), 'targetAttribute' => ['author_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'author_id' => 'Author ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAuthor()
+    {
+        return $this->hasOne(Author::className(), ['id' => 'author_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getRegisters()
+    {
+        return $this->hasMany(Register::className(), ['book_id' => 'id']);
+    }
+}
--- modules/admin/views/default/index.php
+++ modules/admin/views/default/index.php
@@ -3,8 +3,27 @@
     use yii\helpers\Html;
     use yii\helpers\Url;
 
-    echo Html::tag('a', 'Авторы', [ 
+    $authors = Html::tag('a', 'Авторы', [ 
         'href' => Url::toRoute('author/index')
     ]);
+
+    $books = Html::tag('a', 'Книги', [ 
+        'href' => Url::toRoute('book/index')
+    ]);
+
+    echoLink($authors);
+    echoLink($books);
+
+    /**
+     * Выводит строку, оборачивая вывод в тэг <p>
+     * 
+     * @param {string} $a 
+     */
+    function echoLink($a)
+    {
+        echo Html::beginTag('p');
+        echo $a;
+        echo Html::endTag('p');
+    }
 ?>
 </div>
