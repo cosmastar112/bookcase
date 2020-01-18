47d854f34e2cb0e8f83faa007082bc4a6c89cc8e
cosmastar112 <cosmastar112@gmail.com>
2020-01-18 15:10:23 +0400

    Обновление текста на главной странице

--- views/site/index.php
+++ views/site/index.php
@@ -9,30 +9,30 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Прочитал новую книгу?</h1>
+        <h1>Прочитали новую книгу?</h1>
 
-        <p class="lead">Добавь информацию для отслеживания своей читательской статистики.</p>
+        <p class="lead">Добавьте информацию для отслеживания своей читательской статистики.</p>
 
         <p><a class="btn btn-lg btn-success" href= <?= Url::to(['/admin/register/create']) ?>>Добавить запись в регистр</a></p>
 
     </div>
 
-    <h3>Также ты можешь посмотреть...</h2>
+    <h3>Также вы можете просмотреть...</h2>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Таблицы</h2>
+                <h2>Журнал:</h2>
                 <ul>
                     <li>
-                        <a class="" href= <?= Url::to(['/authors/index']) ?> >Авторы</a>
+                        <a class="" href= <?= Url::to(['/authors/index']) ?> >Авторов</a>
                     </li>
                     <li>
-                        <a class="" href= <?= Url::to(['/books/index']) ?> >Книги</a>
+                        <a class="" href= <?= Url::to(['/books/index']) ?> >Книг</a>
                     </li>
                     <li>
-                        <a class="" href= <?= Url::to(['/register/index']) ?> >Регистр</a>
+                        <a class="" href= <?= Url::to(['/register/index']) ?> >Записей о прочтении</a>
                     </li>
                 </ul>
 
@@ -43,7 +43,7 @@ $this->title = 'My Yii Application';
 
             </div>
             <div class="col-lg-4">
-                <h2>Что-то ещё</h2>
+                <h2>...</h2>
             </div>
         </div>
 
